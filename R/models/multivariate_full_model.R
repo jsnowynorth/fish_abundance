2f10ae53a0a1c7a61d1b05ac1262c7baefbc60ef
jsnowynorth <jsnowynorth@gmail.com>
2021-04-28 13:15:27 -0500

    updated sampler for thinning

--- R/models/multivariate_full_model.R
+++ R/models/multivariate_full_model.R
@@ -36,6 +36,8 @@ library(latex2exp)
 library(ggcorrplot)
 library(corrplot)
 library(ggpubr)
+# library(sparseMVN)
+library(Matrix)
 
 
 
@@ -198,7 +200,9 @@ fish_dat = fish_dat %>%
          DOY_sin_semi = sin(DOY/365 * 4*pi),
          DOY_cos_semi = cos(DOY/365 * 4*pi),
          DOY_sin_semi_temp = DOY_sin_semi * temp_0,
-         DOY_cos_semi_temp = DOY_cos_semi * temp_0)
+         DOY_cos_semi_temp = DOY_cos_semi * temp_0) %>% 
+  mutate(DOW = as.factor(DOW)) %>% 
+  arrange(DOW)
 
 
 colnames(fish_dat)
@@ -209,30 +213,14 @@ catch_covs = colnames(fish_dat)[c(24, 27:30)] # temp doy interaction
 gear_types = colnames(fish_dat)[c(21, 22)]
 # need columns: DOW, COMMON_NAME, EFFORT, SURVEYDATE
 
-# ggplot(fish_dat, aes(x = MAX_DEPTH_FEET, y = secchi)) +
-#   geom_point()
-
-# fish_dat %>%
-#   group_by(year) %>%
-#   distinct(DOW, .keep_all = T) %>%
-#   select(all_of(mean_covs)) %>%
-#   ungroup() %>%
-#   mutate_at(vars(mean_covs_log), ~ ifelse(. == 0, . + 0.001, .)) %>%
-#   mutate_at(vars(mean_covs_log), ~ log(.)) %>%
-#   ggpairs()
-# 
-# 
-# fish_dat %>%
-#   group_by(year) %>%
-#   distinct(DOW, .keep_all = T) %>%
-#   select(all_of(mean_covs), all_of(catch_covs)) %>%
-#   ungroup() %>%
-#   mutate_at(vars(all_of(mean_covs_log)), ~ ifelse(. == 0, . + 0.001, .)) %>%
-#   mutate_at(vars(all_of(mean_covs_log)), ~ log(.)) %>%
-#   ggpairs()
-
-
 # model -------------------------------------------------------------------
+
+pars <- create_pars(fish_dat, mean_covs, mean_covs_log, catch_covs)
+
+pars <- update_beta(pars)
+pars <- update_phi(pars)
+pars <- update_omega(pars)
+pars <- update_sigma_species(pars)
 
 create_pars <- function(fish_dat, mean_covs, mean_covs_log, catch_covs){
   
@@ -255,11 +243,13 @@ create_pars <- function(fish_dat, mean_covs, mean_covs_log, catch_covs){
   }
   
   for(k in 1:K){
+ 
     X = fish_dat %>% 
       filter(COMMON_NAME == levs[k]) %>% 
       select(all_of(mean_covs)) %>% 
       mutate_at(vars(all_of(mean_covs_log)), ~ ifelse(. == 0, . + 0.001, .)) %>% 
-      mutate_at(vars(all_of(mean_covs_log)), ~ log(.))
+      mutate_at(vars(all_of(mean_covs_log)), ~ log(.)) %>% 
+      mutate(Int = 1)
     
     Z = fish_dat %>% 
       filter(COMMON_NAME == levs[k]) %>%
@@ -281,9 +271,6 @@ create_pars <- function(fish_dat, mean_covs, mean_covs_log, catch_covs){
   pars$p_phi = ncol(pars$Z[[1]])
   pars$K = K
   
-  pars$beta_0 = 0
-  pars$beta_0_accept = 0
-  pars$beta_0_prior_var = 100
   pars$beta = array(0, dim = c(K, pars$p_beta))
   pars$beta_accept =  array(0, dim = c(K, pars$p_beta))
   pars$beta_prior_var = 100
@@ -296,7 +283,30 @@ create_pars <- function(fish_dat, mean_covs, mean_covs_log, catch_covs){
   pars$nu_species = K + 10
   pars$Psi_species = diag(K)
   
-  pars$omega = matrix(rep(0, K), ncol = K)
+  # pars$omega = matrix(rep(0, K), ncol = K)
+  pars$omega = matrix(0, nrow = n_lakes, ncol = K)
+  
+  # spatial parameters
+  spat_dat = fish_dat %>% 
+    distinct(DOW, .keep_all = T) %>% 
+    select(DOW, LAKE_CENTER_UTM_EASTING, LAKE_CENTER_UTM_NORTHING)
+  
+  d = rdist(cbind(spat_dat$LAKE_CENTER_UTM_EASTING, spat_dat$LAKE_CENTER_UTM_NORTHING))/1000
+  phi = 20
+  pars$Sigma_spatial = exp(-d/phi)
+  pars$Sigma_spatial_inv = solve(pars$Sigma_spatial)
+  # pars$up_chol_spatial = spam::chol(as.spam(pars$Sigma_spatial))
+  pars$up_chol_spatial = t(chol(pars$Sigma_spatial))
+  
+  
+  
+  
+  # spat_dat %>%
+  #   mutate(lake = pars$Sigma_spatial[1000,]) %>%
+  #   ggplot(., aes(x = LAKE_CENTER_UTM_EASTING, y = LAKE_CENTER_UTM_NORTHING, color = lake)) +
+  #   geom_point() +
+  #   scale_color_gradient(low = 'yellow', high = 'red')
+    
   
   # Proposal variances
   pars$sig_prop_beta = array(2, dim = c(K, pars$p_beta))
@@ -335,27 +345,10 @@ update_beta <- function(pars){
   sig_prop_beta = pars$sig_prop_beta
   beta_prior_var = pars$beta_prior_var
   
-  # beta_0 monitor
-  beta_0 = pars$beta_0
-  beta_0_accept = 0
-  beta_0_prior_var = pars$beta_0_prior_var
-  sig_prop_beta_0 = pars$sig_prop_beta_0
-  
-  # beta_0
-  
-  b_prop = rnorm(1, beta_0, sig_prop_beta_0)
-  
-  
-  like_curr = like_prop = 0
-  for(k in 1:K){
-    like_curr = like_curr + sum(dpois(Y[[k]], lambda = effort[[k]]*exp(beta_0 + X[[k]] %*% beta_curr[k,] + Z[[k]] %*% phi[k,] + omega[k]), log = T)) + dnorm(beta_0, 0, sig_prop_beta_0, log = T)
-    like_prop = like_prop + sum(dpois(Y[[k]], lambda = effort[[k]]*exp(b_prop + X[[k]] %*% beta_curr[k,] + Z[[k]] %*% phi[k,] + omega[k]), log = T)) + dnorm(b_prop, 0, sig_prop_beta_0, log = T)
-  }
-  # if(is.na(like_prop) | is.na(like_curr)){print(b_prop, beta_0, beta_curr)}
-  if((like_prop - like_curr) > log(runif(1))){
-    beta_0 = b_prop
-    beta_0_accept = 1
-  }
+  # construct omega
+  ind_array = tibble(id = pars$lake_id, as_tibble(omega, .name_repair = ~LETTERS[1:K]))
+  lake_array = tibble(id = pars$lake_index)
+  OMEGA = as.matrix(lake_array %>% right_join(ind_array, by = 'id') %>% select(-id))
   
   # all other betas
   for(i in 1:p_beta){
@@ -366,10 +359,9 @@ update_beta <- function(pars){
       b_prop = beta_prop[k,i] = rnorm(1, beta_curr[k,i], sig_prop_beta[k,i])
       b_curr = beta_curr[k,i]
       
-      like_curr = sum(dpois(Y[[k]], lambda = effort[[k]]*exp(beta_0 + X[[k]] %*% beta_curr[k,] + Z[[k]] %*% phi[k,] + omega[k]), log = T)) + dnorm(b_curr, 0, beta_prior_var, log = T)
-      like_prop = sum(dpois(Y[[k]], lambda = effort[[k]]*exp(beta_0 + X[[k]] %*% beta_prop[k,] + Z[[k]] %*% phi[k,] + omega[k]), log = T)) + dnorm(b_prop, 0, beta_prior_var, log = T)
+      like_curr = sum(dpois(Y[[k]], lambda = effort[[k]]*exp(X[[k]] %*% beta_curr[k,] + Z[[k]] %*% phi[k,] + OMEGA[,k]), log = T)) + dnorm(b_curr, 0, beta_prior_var, log = T)
+      like_prop = sum(dpois(Y[[k]], lambda = effort[[k]]*exp(X[[k]] %*% beta_prop[k,] + Z[[k]] %*% phi[k,] + OMEGA[,k]), log = T)) + dnorm(b_prop, 0, beta_prior_var, log = T)
       
-      # if(is.na(like_prop) | is.na(like_curr)){print(b_prop, beta_0, beta_curr)}
       if((like_prop - like_curr) > log(runif(1))){
         beta_curr[k,i] = b_prop
         beta_accept[k,i] = 1
@@ -379,8 +371,6 @@ update_beta <- function(pars){
     
   }
   
-  pars$beta_0 = beta_0
-  pars$beta_0_accept = beta_0_accept
   pars$beta = beta_curr
   pars$beta_accept = beta_accept
   
@@ -398,7 +388,6 @@ update_phi <- function(pars){
   effort = pars$effort
   omega = pars$omega
   beta = pars$beta
-  beta_0 = pars$beta_0
   
   # parameters
   n = pars$n
@@ -412,6 +401,11 @@ update_phi <- function(pars){
   sig_prop_phi = pars$sig_prop_phi
   phi_prior_var = pars$phi_prior_var
   
+  # construct omega
+  ind_array = tibble(id = pars$lake_id, as_tibble(omega, .name_repair = ~LETTERS[1:K]))
+  lake_array = tibble(id = pars$lake_index)
+  OMEGA = as.matrix(lake_array %>% right_join(ind_array, by = 'id') %>% select(-id))
+  
   
   # update_phi
   for(i in 1:p_phi){
@@ -422,10 +416,9 @@ update_phi <- function(pars){
       p_prop = phi_prop[k,i] = rnorm(1, phi_curr[k,i], sig_prop_phi[k,i])
       p_curr = phi_curr[k,i]
       
-      like_curr = sum(dpois(Y[[k]], lambda = effort[[k]]*exp(beta_0 + X[[k]] %*% beta[k,] + Z[[k]] %*% phi_curr[k,] + omega[k]), log = T)) + dnorm(p_curr, 0, phi_prior_var, log = T)
-      like_prop = sum(dpois(Y[[k]], lambda = effort[[k]]*exp(beta_0 + X[[k]] %*% beta[k,] + Z[[k]] %*% phi_prop[k,] + omega[k]), log = T)) + dnorm(p_prop, 0, phi_prior_var, log = T)
+      like_curr = sum(dpois(Y[[k]], lambda = effort[[k]]*exp(X[[k]] %*% beta[k,] + Z[[k]] %*% phi_curr[k,] + OMEGA[,k]), log = T)) + dnorm(p_curr, 0, phi_prior_var, log = T)
+      like_prop = sum(dpois(Y[[k]], lambda = effort[[k]]*exp(X[[k]] %*% beta[k,] + Z[[k]] %*% phi_prop[k,] + OMEGA[,k]), log = T)) + dnorm(p_prop, 0, phi_prior_var, log = T)
       
-      # if(is.na(like_prop) | is.na(like_curr)){print(b_prop, beta_0, beta_curr)}
       if((like_prop - like_curr) > log(runif(1))){
         phi_curr[k,i] = p_prop
         phi_accept[k,i] = 1
@@ -443,10 +436,15 @@ update_phi <- function(pars){
   
 }
 
-ll_calc <- function(Y, effort, beta_0, X, beta, Z, phi, omega, K){
+ll_calc <- function(Y, effort, X, beta, Z, phi, omega, mu, K, lake_id, lake_index){
+  
+  ind_array = tibble(id = lake_id, as_tibble(omega, .name_repair = ~LETTERS[1:K]))
+  lake_array = tibble(id = lake_index)
+  OMEGA = as.matrix(lake_array %>% right_join(ind_array, by = 'id') %>% select(-id))
+  
   ll = 0
   for(k in 1:K){
-    ll = ll + sum(dpois(Y[[k]], lambda = effort[[k]]*exp(beta_0 + X[[k]] %*% beta[k,] + Z[[k]] %*% phi[k,] + omega[k]), log = T))
+    ll = ll + sum(dpois(Y[[k]], lambda = effort[[k]]*exp(X[[k]] %*% beta[k,] + Z[[k]] %*% phi[k,] + OMEGA[,k]), log = T))
   }
   return(ll)
 }
@@ -458,31 +456,39 @@ update_omega <- function(pars){
   X = pars$X
   Z = pars$Z
   effort = pars$effort
-  beta_0 = pars$beta_0
+  mu = pars$mu
   beta = pars$beta
   phi = pars$phi
   omega = pars$omega
   Sigma_species = pars$Sigma_species
-  
+  # Sigma_spatial = pars$Sigma_spatial
+  up_chol_spatial = pars$up_chol_spatial
+  lake_id = pars$lake_id
+  lake_index = pars$lake_index
+  n_lakes = pars$n_lakes
+
   # parameters
   n = pars$n
   p = pars$p
   K = pars$K
   
   # choose ellipse
-  nu = rmvnorm(1, rep(0, K), Sigma_species)
-  nu = nu - mean(nu)
+  # U = Matrix::kronecker(Matrix(t(chol(Sigma_species))), up_chol_spatial)
+  U = spam::kronecker.spam( t(chol(Sigma_species)), up_chol_spatial)
+  b = rnorm(n_lakes*K)
+  v = as.matrix(U%*%b)
+  nu = v - mean(v)
   
   # ll threshold
-  logy = ll_calc(Y, effort, beta_0, X, beta, Z, phi, omega, K) + log(runif(1))
+  logy = ll_calc(Y, effort, X, beta, Z, phi, omega, mu, K, lake_id, lake_index) + log(runif(1))
   
   # draw initial proposal
   theta = runif(1, 0, 2*pi)
   theta_min = theta - 2*pi
   theta_max = theta
   
-  f_proposal = omega * cos(theta) + nu * sin(theta)
-  logf = ll_calc(Y, effort, beta_0, X, beta, Z, phi, f_proposal, K)
+  f_proposal = matrix(c(omega) * cos(theta) + nu * sin(theta), ncol = K)
+  logf = ll_calc(Y, effort, X, beta, Z, phi, f_proposal, mu, K, lake_id, lake_index)
   
   if(logf > logy){
     keeper = f_proposal
@@ -495,14 +501,13 @@ update_omega <- function(pars){
         theta_max = theta
       }
       theta = runif(1, theta_min, theta_max)
-      f_proposal = omega * cos(theta) + nu * sin(theta)
-      logf = ll_calc(Y, effort, beta_0, X, beta, Z, phi, f_proposal, K)
+      f_proposal = matrix(c(omega) * cos(theta) + nu * sin(theta), ncol = K)
+      logf = ll_calc(Y, effort, X, beta, Z, phi, f_proposal, mu, K, lake_id, lake_index)
     }
     
     keeper = f_proposal
     
   }
-  
   
   pars$omega = keeper
   
@@ -516,15 +521,18 @@ update_sigma_species <- function(pars){
   # data
   Sigma_species = pars$Sigma_species
   omega = pars$omega
+  mu = pars$mu
+  Sigma_spatial_inv = pars$Sigma_spatial_inv
   
   nu_species = pars$nu_species
   Psi_species = pars$Psi_species
   
   # parameters
   K = pars$K
+  n_lakes = pars$n_lakes
   
-  nu_hat = nu_species + K
-  psi_hat = Psi_species + t(omega) %*% omega
+  nu_hat = nu_species + n_lakes
+  psi_hat = Psi_species + t(omega) %*% Sigma_spatial_inv %*% (omega)
   
   pars$Sigma_species = MCMCpack::riwish(nu_hat, psi_hat)
   
@@ -566,69 +574,81 @@ update_proposal_var_phi <- function(pars, phi_accept_post, i, check_num){
   
 }
 
-update_proposal_var_beta_0 <- function(pars, beta_0_accept_post, i, check_num){
+sampler <- function(fish_dat, mean_covs, mean_covs_log, catch_covs, nits, burnin, thin, check_num = 200, pars = NULL){
   
-  sig_prop = pars$sig_prop_beta_0
-  
-  accept_rate = mean(beta_0_accept_post[(i-check_num+1):i])
-  
-  if(accept_rate < 0.2){
-    sig_prop = sig_prop*0.9
-  }else if(accept_rate > 0.45){
-    sig_prop = sig_prop/0.9
-  }else{
-    sig_prop = sig_prop
+  if(is.null(pars)){
+    pars <- create_pars(fish_dat, mean_covs, mean_covs_log, catch_covs)
   }
   
-  pars$sig_prop_beta_0 = sig_prop
-  
-  return(pars)
-  
-  
-}
-
-sampler <- function(fish_dat, mean_covs, mean_covs_log, catch_covs, nits, check_num = 200){
-  
-  pars <- create_pars(fish_dat, mean_covs, mean_covs_log, catch_covs)
   
   p_beta = pars$p_beta
   p_phi = pars$p_phi
   K = pars$K
   
-  beta_post = array(NA, dim = c(K, p_beta, nits))
+  keep_samp = nits-burnin
+  keep_num = length(seq(burnin, nits, by = thin))
+  
+  beta_post = array(NA, dim = c(K, p_beta, keep_num))
   beta_accept_post = array(NA, dim = c(K, p_beta, nits))
-  phi_post = array(NA, dim = c(K, p_phi, nits))
+  phi_post = array(NA, dim = c(K, p_phi, keep_num))
   phi_accept_post = array(NA, dim = c(K, p_phi, nits))
-  beta_0_post = array(NA, dim = c(nits))
-  beta_0_accept_post = array(NA, dim = c(nits))
-  omega_post = array(NA, dim = c(length(pars$omega), nits))
-  sigma_species_post = array(NA, dim = c(dim(pars$Sigma_species), nits))
+  omega_post = array(NA, dim = c(length(pars$omega), keep_num))
+  sigma_species_post = array(NA, dim = c(dim(pars$Sigma_species), keep_num))
+  
   
   pb <- progress_bar$new(
-    format = "  Running [:bar] :percent eta: :eta",
-    total = nits, clear = FALSE, width = 60)
+    format = "Burnin Running [:bar] :percent eta: :eta",
+    total = (burnin - 1), clear = FALSE, width = 60)
   
-  for(i in seq(1, nits)){
+  for(i in seq(1, (burnin - 1))){
     
     pars <- update_beta(pars)
     pars <- update_phi(pars)
     pars <- update_omega(pars)
     pars <- update_sigma_species(pars)
     
-    beta_post[,,i] = pars$beta
     beta_accept_post[,,i] = pars$beta_accept
-    phi_post[,,i] = pars$phi
     phi_accept_post[,,i] = pars$phi_accept
-    beta_0_post[i] = pars$beta_0
-    beta_0_accept_post[i] = pars$beta_0_accept
-    omega_post[,i] = pars$omega
-    sigma_species_post[,,i] = pars$Sigma_species
     
-    
-    if(i %in% seq(0, nits-1, by = check_num)){
+    if(i %in% seq(0, burnin-1, by = check_num)){
       pars <- update_proposal_var_beta(pars, beta_accept_post, i, check_num)
       pars <- update_proposal_var_phi(pars, phi_accept_post, i, check_num)
-      pars <- update_proposal_var_beta_0(pars, beta_0_accept_post, i, check_num)
+    }
+    
+    pb$tick()
+    
+  }
+  
+  
+  pb <- progress_bar$new(
+    format = "  Running [:bar] :percent eta: :eta",
+    total = (keep_samp + 1), clear = FALSE, width = 60)
+  
+  j=1
+  for(i in seq(burnin, nits)){
+    
+    pars <- update_beta(pars)
+    pars <- update_phi(pars)
+    pars <- update_omega(pars)
+    pars <- update_sigma_species(pars)
+    
+    beta_accept_post[,,i] = pars$beta_accept
+    phi_accept_post[,,i] = pars$phi_accept
+    
+    if(i %in% seq(burnin, nits, by = thin)){
+      
+      beta_post[,,j] = pars$beta
+      phi_post[,,j] = pars$phi
+      omega_post[,j] = pars$omega
+      sigma_species_post[,,j] = pars$Sigma_species
+      
+      j = j+1
+      
+    }
+    
+    if(i %in% seq(burnin, nits, by = check_num)){
+      pars <- update_proposal_var_beta(pars, beta_accept_post, i, check_num)
+      pars <- update_proposal_var_phi(pars, phi_accept_post, i, check_num)
     }
     
     
@@ -642,27 +662,20 @@ sampler <- function(fish_dat, mean_covs, mean_covs_log, catch_covs, nits, check_
               phi_accept = phi_accept_post,
               sig_prop_beta = pars$sig_prop_beta,
               sig_prop_phi = pars$sig_prop_phi,
-              beta_0 = beta_0_post,
-              beta_0_accept = beta_0_accept_post,
-              sig_prop_beta_0 = pars$sig_prop_beta_0,
               omega = omega_post,
               sig_prop_omega = pars$sig_prop_omega,
-              sigma_species = sigma_species_post))
+              sigma_species = sigma_species_post,
+              pars = pars))
   
 }
 
-run = sampler(fish_dat, mean_covs, mean_covs_log, catch_covs, nits = 50000, check_num = 100)
+run = sampler(fish_dat, mean_covs, mean_covs_log, catch_covs, nits = 50000, burnin = 25000, thin = 10, check_num = 200, pars = NULL)
 
-# saveRDS(run, file = '/Users/joshuanorth/Desktop/full_model_w_doy_temp_int.rds')
-# saveRDS(run, file = '/Users/joshuanorth/Desktop/full_model_w_doy_temp_int_cov.rds')
-# saveRDS(run, file = '/Users/joshuanorth/Desktop/full_model_w_doy_temp_int_cov_corrected.rds')
-# run = readRDS('/Users/joshuanorth/Desktop/full_model_w_doy_temp_int.rds')
-# run = readRDS('/Users/joshuanorth/Desktop/full_model_w_doy_temp_int_cov.rds')
-run = readRDS('/Users/joshuanorth/Desktop/full_model_w_doy_temp_int_cov_corrected.rds')
-
+# saveRDS(run, file = '/Users/joshuanorth/Desktop/full_model.rds')
+# run = readRDS('/Users/joshuanorth/Desktop/full_model.rds')
 
 # nits = 10000
-burnin = 1:25000
+burnin = 1:40000
 
 apply(run$beta[,,-burnin], c(1,2), mean)
 apply(run$beta_accept[,,-burnin], c(1,2), mean)
@@ -685,19 +698,19 @@ rownames(cmat) = pars$fish_names
 cmat
 
 par(mfrow = c(3,4))
-for(i in 1:11){
-  plot(run$beta[1,i,-burnin], type = 'l', main = b_names[i])
+for(i in 1:12){
+  plot(run$beta[3,i,-burnin], type = 'l', main = b_names[i])
 }
 
 
 par(mfrow = c(3,4))
 for(i in 1:11){
-  plot(run$phi[1,i,-burnin], type = 'l', main = phi_names[i])
+  plot(run$phi[3,i,-burnin], type = 'l', main = phi_names[i])
 }
 
 
-par(mfrow = c(2,3))
-for(i in 1:6){
+par(mfrow = c(3,6))
+for(i in 1:18){
   plot(run$omega[i,-burnin], type = 'l', main = pars$fish_names[i])
 }
 
